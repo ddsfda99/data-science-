0352d0d44d2b756c9e962e6c03c78ababbda71c5
ddsfda99 <168000702+ddsfda99@users.noreply.github.com>
2024-10-28 18:25:24 +0800

    Update 张子彤-10235102511-10月28日作业.py

--- 张子彤-10235102511-10月28日作业.py
+++ 张子彤-10235102511-10月28日作业.py
@@ -1,4 +1,4 @@
-mport pandas as pd
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
